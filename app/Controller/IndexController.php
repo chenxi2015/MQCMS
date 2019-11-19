7410100a27f6353d99b04203f0f9c6fdf19951a3
chenxi <bbxycx.18@163.com>
2019-11-19 14:06:08 +0800

    init

---
+++ app/Controller/IndexController.php
@@ -0,0 +1,38 @@
+<?php
+
+declare(strict_types=1);
+/**
+ * This file is part of Hyperf.
+ *
+ * @link     https://www.hyperf.io
+ * @document https://doc.hyperf.io
+ * @contact  group@hyperf.io
+ * @license  https://github.com/hyperf-cloud/hyperf/blob/master/LICENSE
+ */
+
+namespace App\Controller;
+use Hyperf\HttpServer\Annotation\Controller;
+use Hyperf\HttpServer\Annotation\RequestMapping;
+
+/**
+ * @Controller()
+ * Class IndexController
+ * @package App\Controller
+ */
+class IndexController extends AbstractController
+{
+    /**
+     * @RequestMapping(path="index", methods="get, post")
+     * @return array
+     */
+    public function index()
+    {
+        $user = $this->request->input('user', 'Hyperf');
+        $method = $this->request->getMethod();
+
+        return [
+            'method' => $method,
+            'message' => "Hello {$user}.",
+        ];
+    }
+}
